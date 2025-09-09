1b8d11c375e1a297ddc6738f0b23dde65cb54edc
SANSKAR SHARMA <113014922+SANSKAR-1999@users.noreply.github.com>
2025-09-09 12:46:32 +0530

    Update next-permutation.py

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -1,25 +1,15 @@
-class Solution(object):
-    def nextPermutation(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        
-        i = len(nums) - 2
-        while i >= 0 and nums[i] >= nums[i + 1]:
-            i -= 1
-        
-        if i >= 0:
-            j = len(nums) - 1
-            while j > i and nums[j] <= nums[i]:
-                j -= 1
-            
-            nums[i], nums[j] = nums[j], nums[i]
-        
-        left = i + 1
-        right = len(nums) - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+def nextPermutation(nums):
+
+    i = len(nums) - 2
+    while i >= 0 and nums[i] >= nums[i + 1]:
+        i -= 1
+    if i >= 0:
+        j = len(nums) - 1
+        while nums[j] <= nums[i]:
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]
+    return nums
+
+
+print(nextPermutation(nums=[1, 1, 5]))
         

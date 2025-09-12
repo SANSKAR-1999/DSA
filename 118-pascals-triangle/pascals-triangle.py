843255c73924d11034a184b7795a87e575be683e
SANSKAR SHARMA <113014922+SANSKAR-1999@users.noreply.github.com>
2025-09-12 16:23:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (20.33%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -9,10 +9,10 @@ class Solution(object):
         triangle = [[1]]
         for i in range(1, numRows):
             prev_row = triangle[-1]
-            new_row = [1]  # Indented: Starts each new row
-            for j in range(1, i):  # Indented: Builds middles for this row
+            new_row = [1]  
+            for j in range(1, i):
                 new_row.append(prev_row[j-1] + prev_row[j])
-            new_row.append(1)  # Indented: Ends each new row
-            triangle.append(new_row)  # Indented: Adds this row to triangle
+            new_row.append(1) 
+            triangle.append(new_row)  
         return triangle
 
